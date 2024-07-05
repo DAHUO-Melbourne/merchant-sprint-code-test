fc9f6f36ea60c1580c7c0da1690a3afc2258e13a
DaHuo-Melbourne <huodamelbourne@gmail.com>
2024-07-05 23:19:48 +1000

    feat: initialize the eslint and prettier

--- coding-challenge-api/src/index.ts
+++ coding-challenge-api/src/index.ts
@@ -1,15 +1,15 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 
-import cors from "cors";
-import { getUser } from "./user";
+import cors from 'cors';
+import { getUser } from './user';
 
 const app = express();
 const port = 8080;
 
 app.use(cors());
-app.get("/user", getUser);
+app.get('/user', getUser);
 
-app.get("/sales", (req, res) => {
+app.get('/sales', (req, res) => {
   /** Write an api for the widget */
 });
 
