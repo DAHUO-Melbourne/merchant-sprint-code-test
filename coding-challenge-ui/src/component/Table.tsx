9addbeeb638af2dfd04838c35c1e8327f7fc6597
DaHuo-Melbourne <huodamelbourne@gmail.com>
2024-07-07 01:51:58 +1000

    feat: adjust table head and row styles

--- coding-challenge-ui/src/component/Table.tsx
+++ coding-challenge-ui/src/component/Table.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import MaterialTable from 'material-table';
 import { Order } from '../page/Dashboard';
+import './OverdueOrdersTable.css';
 
 interface OverdueOrdersTableProps {
   orders: Order[];
@@ -8,32 +9,72 @@ interface OverdueOrdersTableProps {
 
 const OverdueOrdersTable: React.FC<OverdueOrdersTableProps> = ({ orders }) => {
   return (
-    <MaterialTable
-      title="Overdue Orders"
-      columns={[
-        { title: 'MARKETPLACE', field: 'storeMarketplace' },
-        { title: 'STORE', field: 'storeShopName' },
-        { title: 'ORDER ID', field: 'orderId' },
-        { title: 'ORDER VALUE', field: 'orderValue' },
-        { title: 'ITEMS', field: 'items', type: 'numeric' },
-        { title: 'DESTINATION', field: 'destination' },
-        {
-          title: 'DAYS OVERDUE',
-          field: 'daysOverdue',
-          type: 'numeric',
-          render: (rowData) => (
-            <span style={{ color: rowData.daysOverdue > 20 ? 'red' : 'black' }}>
-              {rowData.daysOverdue}
-            </span>
-          ),
-        },
-      ]}
-      data={orders}
-      options={{
-        paging: true,
-        pageSize: 5,
-      }}
-    />
+    <div className="table-container">
+      <MaterialTable
+        title="Overdue Orders"
+        columns={[
+          {
+            title: 'MARKETPLACE',
+            field: 'storeMarketplace',
+          },
+          {
+            title: 'STORE',
+            field: 'storeShopName',
+          },
+          {
+            title: 'ORDER ID',
+            field: 'orderId',
+          },
+          {
+            title: 'ORDER VALUE',
+            field: 'orderValue',
+          },
+          {
+            title: 'ITEMS',
+            field: 'items',
+            type: 'numeric',
+          },
+          {
+            title: 'DESTINATION',
+            field: 'destination',
+            headerStyle: {
+              fontSize: 14,
+              fontWeight: 600,
+            },
+            cellStyle: {
+              fontSize: 12,
+            },
+          },
+          {
+            title: 'DAYS OVERDUE',
+            field: 'daysOverdue',
+            type: 'numeric',
+            render: (rowData) => (
+              <span
+                style={{ color: rowData.daysOverdue > 20 ? 'red' : 'black' }}
+              >
+                {rowData.daysOverdue}
+              </span>
+            ),
+          },
+        ]}
+        data={orders}
+        options={{
+          paging: true,
+          pageSize: 5,
+          search: false,
+          headerStyle: {
+            fontSize: 12,
+            color: '#ABB2B9',
+            fontWeight: 600,
+            backgroundColor: '#F2F3F4',
+          },
+          rowStyle: {
+            fontSize: 12,
+          },
+        }}
+      />
+    </div>
   );
 };
 
