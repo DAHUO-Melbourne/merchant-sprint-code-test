a1365dace80e0b92f45b4d621c5b5f9eafd4e8e8
DaHuo-Melbourne <huodamelbourne@gmail.com>
2024-07-07 01:12:15 +1000

    feat: add initial data rendering in the table

--- coding-challenge-ui/src/component/Table.tsx
+++ coding-challenge-ui/src/component/Table.tsx
@@ -1,25 +1,40 @@
 import React from 'react';
 import MaterialTable from 'material-table';
+import { Order } from '../page/Dashboard';
 
-const Table = () => {
+interface OverdueOrdersTableProps {
+  orders: Order[];
+}
+
+const OverdueOrdersTable: React.FC<OverdueOrdersTableProps> = ({ orders }) => {
   return (
     <MaterialTable
+      title="Overdue Orders"
       columns={[
-        { title: 'Adı', field: 'name' },
-        { title: 'Soyadı', field: 'surname' },
-        { title: 'Doğum Yılı', field: 'birthYear', type: 'numeric' },
+        { title: 'MARKETPLACE', field: 'storeMarketplace' },
+        { title: 'STORE', field: 'storeShopName' },
+        { title: 'ORDER ID', field: 'orderId' },
+        { title: 'ORDER VALUE', field: 'orderValue' },
+        { title: 'ITEMS', field: 'items', type: 'numeric' },
+        { title: 'DESTINATION', field: 'destination' },
         {
-          title: 'Doğum Yeri',
-          field: 'birthCity',
-          lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+          title: 'DAYS OVERDUE',
+          field: 'daysOverdue',
+          type: 'numeric',
+          render: (rowData) => (
+            <span style={{ color: rowData.daysOverdue > 20 ? 'red' : 'black' }}>
+              {rowData.daysOverdue}
+            </span>
+          ),
         },
       ]}
-      data={[
-        { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-      ]}
-      title="Demo Title"
+      data={orders}
+      options={{
+        paging: true,
+        pageSize: 5,
+      }}
     />
   );
 };
 
-export default Table;
+export default OverdueOrdersTable;
--- coding-challenge-ui/src/page/Dashboard.tsx
+++ coding-challenge-ui/src/page/Dashboard.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Table from '../component/Table';
 
+export interface Order {
+  orderId: string;
+  orderValue: string;
+  items: string;
+  destination: string;
+  daysOverdue: number;
+  storeCountry: string;
+  storeMarketplace: string;
+  storeShopName: string;
+}
+
 const Dashboard = () => {
-  return <Table />;
+  const [orders, setOrders] = useState<Order[]>([]);
+  useEffect(() => {
+    fetch(
+      'http://localhost:8080/orders/overdueOrders?order=desc&pageSize=5&skip=0',
+    )
+      .then((results) => results.json())
+      .then((data) => {
+        setOrders(data);
+      });
+  }, []);
+  return <Table orders={orders} />;
 };
 
 export default Dashboard;
